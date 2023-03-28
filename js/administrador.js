4de2a069c21b8f3498fb1f81a1b1cd5c0c31cbcb
Alvaro Capaceta <ingcapadev@gmail.com>
2023-03-27 23:58:24 -0700

    Axios Productos Buscar por ID

--- js/administrador.js
+++ js/administrador.js
@@ -132,5 +132,73 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  const obtenerDatosBuscar = () => {
+    return document.getElementById('form-search-id').value;
+  };
+
+  const buscarForm = document.getElementById('searchForm');
+  buscarForm.onsubmit = async (event) => {
+    event.preventDefault();
+    const table = document.getElementById('tableData');
+    const productTemplate = document.getElementById('productTemplate');
+    const fragment = document.createDocumentFragment();
+    const id = obtenerDatosBuscar();
+    const url = `${baseUrl}${id}`;
+    const alert = document.getElementById('alert-search');
+
+    try {
+      let { data } = await axios.get(url);
+      data = data.data;
+      const clone = productTemplate.content.cloneNode(true);
+
+      const price = data.price.toFixed(3);
+      clone.querySelector('#id').textContent = data.id;
+      clone.querySelector('#name').textContent = data.name;
+      clone.querySelector('#price').textContent = `$ ${price}`;
+      clone.querySelector('#editorial').textContent = data.editorial;
+      if (data.img != null) {
+        clone.querySelector('#img').src = data.img;
+      }
+      if (data.img == null) {
+        clone.querySelector('#img').src = '/img/defaultImage.jpg';
+      }
+      clone.querySelector('#year').textContent = data.year;
+
+      clone.querySelector('.edit').addEventListener('click', () => {
+        document.querySelector('#form-edit-id').value = data.id;
+        document.querySelector('#nombre-edit').value = data.name;
+        document.querySelector('#precio-edit').value = data.price;
+        document.querySelector('#editorial-edit').value = data.editorial;
+        if (data.img != null) {
+          document.querySelector('#img-edit').src = data.img;
+        }
+        if (data.img == null) {
+          document.querySelector('#img-edit').src = '/img/defaultImage.jpg';
+        }
+        document.querySelector('#year-edit').value = data.year;
+        $('#editEmployeeModal').modal();
+      });
+
+      clone.querySelector('.delete').addEventListener('click', () => {
+        document.querySelector('#form-delete-id').value = data.id;
+        $('#deleteEmployeeModal').modal();
+      });
+
+      fragment.appendChild(clone);
+      table.innerHTML = '';
+      table.appendChild(fragment);
+      $('#searchEmployeeModal').modal('hide');
+    } catch (error) {
+      alert.hidden = false;
+      alert.innerHTML = error.response.data.error;
+    }
+  };
+
+  $('#searchEmployeeModal').on('hidden.bs.modal', () => {
+    document.getElementById('alert-search').hidden = true;
+    document.getElementById('alert-search').innerHTML = '';
+    document.getElementById('form-search-id').value = '';
+  });
+
   productRender();
 });
