9f623315f374fb24cd6a2d1688d8655c4cbfd115
Alvaro Capaceta <ingcapadev@gmail.com>
2023-04-14 21:56:21 -0700

    Checkout compra pintar carrito

--- js/checkout.js
+++ js/checkout.js
@@ -1,21 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const carrito = JSON.parse(localStorage.getItem('carrito'));
-  const nPrecio = Object.values(carrito).reduce(
-    (acc, { cantidad, price }) => acc + cantidad * price,
-    0
-  );
+  function pintarCarrito() {
+    const carrito = JSON.parse(localStorage.getItem('carrito'));
+    const nPrecio = Object.values(carrito).reduce(
+      (acc, { cantidad, price }) => acc + cantidad * price,
+      0
+    );
+    const nCantidad = Object.values(carrito).reduce(
+      (acc, { cantidad }) => acc + cantidad,
+      0
+    );
+    document.getElementById('total-mxn').textContent = nPrecio;
 
-  document.getElementById('total-mxn').textContent = nPrecio;
+    document.getElementById('carrito-cantidad').textContent = nCantidad;
+    const contenedorProductos = document.getElementById('productsContainer');
+    const productosTemplate = document.getElementById('productTemplate');
+    const fragment = document.createDocumentFragment();
+    const totalclone = document.getElementById('total-li').cloneNode(true);
+    for (const data of Object.values(carrito)) {
+      const clone = productosTemplate.content.cloneNode(true);
 
-  document.getElementById('carrito-cantidad').textContent =
-    Object.keys(carrito).length;
+      clone.querySelector('#product-name').textContent = data.name;
+      clone.querySelector('#product-price').textContent = data.cantidad;
+
+      fragment.appendChild(clone);
+    }
+
+    fragment.appendChild(totalclone);
+    contenedorProductos.innerHTML = '';
+    contenedorProductos.append(fragment);
+  }
 
   async function createSale(event) {
-    event.preventDefault();
-    const pay = document.getElementById('paymentMethod').value;
-    console.log(pay);
+    try {
+      event.preventDefault();
+      const pay = document.getElementById('paymentMethod').value;
+      const carrito = JSON.parse(localStorage.getItem('carrito'));
+      const nPrecio = Object.values(carrito).reduce(
+        (acc, { cantidad, price }) => acc + cantidad * price,
+        0
+      );
+    } catch (error) {
+      window.alert('Ocurrio un error');
+      console.log(error);
+    }
   }
 
   const formCheckout = document.getElementById('form-checkout');
   formCheckout.addEventListener('submit', createSale);
+  pintarCarrito();
 });
